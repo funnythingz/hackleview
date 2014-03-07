03bb96efd38fd736270889fe4271a10cf2a83a7f
funnythingz <hiroki@hackleberry.me>
2014-03-07 16:54:04 +0900

    とりあえず

--- src/hackleview.ts
+++ src/hackleview.ts
@@ -1,9 +1,35 @@
+/// <reference path="../definitions/jquery.d.ts" />
+
 module HACKLE {
+
+    export interface IViewCreateOptions {
+        $el?: JQuery;
+        tagName?: string;
+        id?: string;
+        className?: string;
+        attributes?: Object;
+    }
 
     export class View {
 
-        constructor(public id: string) {}
-        
+        tagName: string = 'div';
+        id: string;
+        className: string;
+        attributes: Object = {};
+        $el: JQuery = $('<' + this.tagName + '>');
+
+        constructor(viewCreateOptions: IViewCreateOptions = {}) {
+            this.tagName = viewCreateOptions.tagName || 'div';
+            this.id = viewCreateOptions.id || '';
+            this.className = viewCreateOptions.className || '';
+            this.attributes = viewCreateOptions.attributes || {};
+            this.$el = this.isJQuery(viewCreateOptions.$el) ? viewCreateOptions.$el : $('<' + this.tagName + '>');
+        }
+
+        private isJQuery($that): boolean {
+            return $that instanceof jQuery;
+        }
+
     }
 
 }
