b906ec2ac6c3dd1516c100aa8c3a43b5a19785a5
funnythingz <hiroki@hackleberry.me>
2014-03-08 23:03:34 +0900

    wrote test for attributes

--- tests/hackleview-spec.ts
+++ tests/hackleview-spec.ts
@@ -13,24 +13,46 @@ module HACKLE {
 
             var div: View = new View();
 
-            it("`div.$el` to instanceof `jQuery`", function() {
+            it("div.$el to instanceof `jQuery`", function() {
                 expect(div.$el).to.instanceof(jQuery);
             });
 
-            it("`div.$el[0]` has `DIV` of `tagName`", function() {
-                expect(div.$el[0].tagName).to.equal('DIV');
+            it("div.$el[0] has `DIV` of tagName", function() {
+                expect(div.$el.get(0).tagName).to.equal('DIV');
             });
 
-            it("`div.$el[0]` has not `id`", function() {
-                expect(div.$el[0].id).to.equal('');
+            it("div.$el[0] has not id", function() {
+                expect(div.$el.get(0).id).to.equal('');
             });
 
-            it("`div.$el[0]` has not any classList", function() {
-                expect(div.$el[0].classList).to.have.length(0);
+            it("div.$el[0] has not any classList", function() {
+                expect(div.$el.get(0).classList).to.have.length(0);
             });
 
-            it("`div.$el[0]` has not any attributes", function() {
-                expect(div.$el[0].attributes).to.have.length(0);
+            it("div.$el[0] has not any attributes", function() {
+                expect(div.$el.get(0).attributes).to.have.length(0);
+            });
+
+        });
+
+        describe("has any attribute on the instance", function() {
+
+            var p: View = new View({
+                              tagName: 'p',
+                              className: 'poge',
+                              id: 'pid'
+                          });
+
+            it("p.$el has `P` of tagName", function() {
+                expect(p.$el.get(0).tagName).to.equal('P');
+            });
+
+            it("p.$el has `poge` class of attribute", function() {
+                expect(p.$el.get(0).className).to.equal('poge');
+            });
+
+            it("p.$el has `pid` id of attribute", function() {
+                expect(p.$el.get(0).id).to.equal('pid');
             });
 
         });
